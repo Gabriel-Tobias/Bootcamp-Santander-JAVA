12b395ec0f5de9e2c42adb13a662278c4f78daf5
Gabriel Tobias <102703941+Gabriel-Tobias@users.noreply.github.com>
2023-08-25 00:40:57 -0300

    Add files via upload

---
+++ java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double PI = 3.14; //constante
+        System.out.print(numero);
+    }
+}
